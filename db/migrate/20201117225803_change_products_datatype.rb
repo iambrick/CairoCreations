58fe0673ff284ea1111a0cd8a19407fa21624830
Brock <b.mykietowich7@gmail.com>
2020-11-17 17:00:29 -0600

    Fixed price to be stored as a decimal

---
+++ db/migrate/20201117225803_change_products_datatype.rb
@@ -0,0 +1,5 @@
+class ChangeProductsDatatype < ActiveRecord::Migration[6.0]
+  def change
+    change_column :products, :price, :decimal
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_224204) do
+ActiveRecord::Schema.define(version: 2020_11_17_225803) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_224204) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
-    t.integer "price"
+    t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
