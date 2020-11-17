2c7e50de2f0f5e3a013c79c60e923e0aec071407
Brock <b.mykietowich7@gmail.com>
2020-11-17 16:36:37 -0600

    Allow admins to add products

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :quantity, :price
+  permit_params :name, :quantity, :price
   #
   # or
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
