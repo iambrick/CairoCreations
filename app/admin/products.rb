49b4e11a198ce8baa7b3d799698640cb0f9da0cd
Brock <b.mykietowich7@gmail.com>
2020-11-17 16:43:24 -0600

    Added descriptions to products table

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :quantity, :price
+  permit_params :name, :quantity, :price, :description
   #
   # or
   #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_212046) do
+ActiveRecord::Schema.define(version: 2020_11_17_224204) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_212046) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
 end
