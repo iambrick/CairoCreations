3098507f49fadf1a3accf5c470e44b540d769096
Brock <b.mykietowich7@gmail.com>
2020-11-23 00:39:46 -0600

    Ransack search now works properly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,6 @@
 class PagesController < ApplicationController
   def search
-    if params[:search].blank?
-      @parameter = "zzzzzzzzzzzzz" #this should never show up
-    else
-      @parameter = params[:search].downcase
-    end
-    @nameResults = Product.all.where("lower(name) LIKE :search", search: "%#{@parameter}%")
-    @descResults = Product.all.where("lower(description) LIKE :search", search: "%#{@parameter}%")
-
-    @allEmpty = @nameResults.blank? && @descResults.blank?
+    @q = Product.ransack(params[:q])
+    @products = @q.result
   end
 end
