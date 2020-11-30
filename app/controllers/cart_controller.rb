50f109ccd1ec701e7366dea17ab2af51b554891b
Brock <b.mykietowich7@gmail.com>
2020-11-30 00:10:56 -0600

    Items can be added to the cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,6 +3,12 @@ class CartController < ApplicationController
   # Data sent as form data (params)
   def create
     # Add params[:id] to cart
+    id = params[:id].to_i
+    unless session[:shopping_cart].include?(id)
+      session[:shopping_cart] << id
+      flash[:notice] = "Added to cart!"
+    end
+    redirect_to root_path
   end
 
   # DELETE /cart/:id
