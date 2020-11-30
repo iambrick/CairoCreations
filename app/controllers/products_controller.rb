31654e3a26b2b4343cdbb125a4bcdbb4e9247f92
Brock <b.mykietowich7@gmail.com>
2020-11-29 22:50:22 -0600

    Styled partials and organized pages.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @Products = Product.all.page(params[:page]).per(10)
+    @products = Product.all.page(params[:page]).per(10)
   end
 
   def show
