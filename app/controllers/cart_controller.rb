5a799face5f2e60cc1e4c891c3612ce0e8f19010
Brock <b.mykietowich7@gmail.com>
2020-11-30 00:26:35 -0600

    Can remove items from the shopping cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -6,7 +6,8 @@ class CartController < ApplicationController
     id = params[:id].to_i
     unless session[:shopping_cart].include?(id)
       session[:shopping_cart] << id
-      flash[:notice] = "Added to cart!"
+      product = Product.find(id)
+      flash[:notice] = "#{product.name} added to cart!"
     end
     redirect_to root_path
   end
@@ -14,5 +15,11 @@ class CartController < ApplicationController
   # DELETE /cart/:id
   def destroy
     # Remove params[:id] from the cart.
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    product = Product.find(id)
+    flash[:notice] = "#{product.name} removed from cart!"
+
+    redirect_to root_path
   end
 end
