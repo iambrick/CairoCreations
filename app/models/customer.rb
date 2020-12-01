b6c75313d7771e15a90c24de297a934f5649f0f8
Brock <b.mykietowich7@gmail.com>
2020-11-30 20:16:01 -0600

    Fixed validations for customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,5 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, :password, presence:true
-  validates :username, uniqueness:true
+  validates :password, presence:true
 end
