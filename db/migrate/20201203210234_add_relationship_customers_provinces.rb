145b1a342bb5b7523d528bf067923e3c4b88f97e
Brock <b.mykietowich7@gmail.com>
2020-12-03 15:03:32 -0600

    Adding relationship between customers and provinces

---
+++ db/migrate/20201203210234_add_relationship_customers_provinces.rb
@@ -0,0 +1,5 @@
+class AddRelationshipCustomersProvinces < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :customers, :province, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_202915) do
+ActiveRecord::Schema.define(version: 2020_12_03_210234) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -75,7 +75,9 @@ ActiveRecord::Schema.define(version: 2020_12_03_202915) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "province_id"
     t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["province_id"], name: "index_customers_on_province_id"
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
@@ -122,4 +124,5 @@ ActiveRecord::Schema.define(version: 2020_12_03_202915) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "customers", "provinces"
 end
