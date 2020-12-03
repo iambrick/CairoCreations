cf37acd4cfb51ae26ae899b49dad9881b409d6ac
Brock <b.mykietowich7@gmail.com>
2020-12-03 17:28:42 -0600

    Registered users can add an address and province to their account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :initialize_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :cart
+
+  protected
+
+  def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:address, :email, :password)}
+
+       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:address, :email, :password, :current_password, :province_id)}
+  end
 
   private
   def initialize_session
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_210234) do
+ActiveRecord::Schema.define(version: 2020_12_03_230216) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_12_03_210234) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "province_id"
+    t.string "address"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["province_id"], name: "index_customers_on_province_id"
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
