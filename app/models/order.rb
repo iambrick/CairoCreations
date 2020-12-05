274e1799f976f94fb7768c5b1f8b6cdfd969d2ec
Brock <b.mykietowich7@gmail.com>
2020-12-04 18:11:46 -0600

    Added taxes to the orders model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
-  validates :customer_id, :status, :created_at, presence:true
+  validates :customer_id, :status, presence:true
   belongs_to :customer
-  has_many :ProductOrders
+  has_many :product_orders
+  has_many :products, through: :product_orders
 end
