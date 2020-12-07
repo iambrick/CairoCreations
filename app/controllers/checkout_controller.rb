9a90bb36837c8e536dff396ab83eb3f5cf39b0f5
Brock <b.mykietowich7@gmail.com>
2020-12-07 14:49:11 -0600

    Checkout controller with associated routes.

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,13 @@
+class CheckoutController < ApplicationController
+  def create
+    #establish a connection with Stripe
+  end
+
+  def success
+    #took money
+  end
+
+  def cancel
+    #something went wrong
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show]
   resources :cart, only: [:create, :destroy]
 
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
+
   root to: 'products#index'
   get '/search' => 'pages#search', :as => 'search_page'
   devise_for :admin_users, ActiveAdmin::Devise.config
